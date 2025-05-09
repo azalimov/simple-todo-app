df1504564215df5062092069587e6c434f556b6f
Azamat Alimov <retq@dsquared.local>
2025-05-09 15:05:13 +0500

    Init commit

---
+++ simple-todo-app/simple_todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_todo_appApp.swift
+//  simple-todo-app
+//
+//  Created by Azamat Alimov on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct simple_todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
